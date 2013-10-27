da0a29c9178f942543f85546a79b6383651d33c1
Wellington Pinheiro <wellington.pinheiro@gmail.com>
2013-10-26 22:29:02 -0200

    initial commit

---
+++ moduloA/src/main/java/moduloA/Interface.java
@@ -0,0 +1,5 @@
+package moduloA;
+
+public interface Interface {
+    public String helloWorld();
+}
---
+++ moduloB/src/main/java/moduloB/InterfaceImpl.java
@@ -0,0 +1,9 @@
+package moduloB;
+
+import moduloA.Interface;
+
+public class InterfaceImpl implements Interface {
+  public String helloWorld() {
+    return "Hello World!";
+  }
+}
